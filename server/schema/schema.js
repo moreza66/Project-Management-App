bb9af79085171e509cfbcb940effa9d3daae94b1
mohammadreza memar <moreza.memar@gmail.com>
2023-04-20 12:45:27 -0500

    starting project with graphql

---
+++ server/schema/schema.js
@@ -0,0 +1,56 @@
+const { projects, clients } = require("../sampleData.js");
+
+const {
+  GraphQLInputObjectType,
+  GraphQLID,
+  GraphQLString,
+  GraphQLObjectType,
+  GraphQLSchema,
+} = require("graphql");
+
+// Client Type
+const ClientType = new GraphQLObjectType({
+  name: "Client",
+  fields: () => ({
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    email: { type: GraphQLString },
+    phone: { type: GraphQLString },
+  }),
+});
+
+const RootQuery = new GraphQLObjectType({
+  name: "RootQueryType",
+  fields: {
+    //     projects: {
+    //       type: new GraphQLList(ProjectType),
+    //       resolve(parent, args) {
+    //         return Project.find();
+    //       },
+    //     },
+    //     project: {
+    //       type: ProjectType,
+    //       args: { id: { type: GraphQLID } },
+    //       resolve(parent, args) {
+    //         return Project.findById(args.id);
+    //       },
+    //     },
+    //     clients: {
+    //       type: new GraphQLList(ClientType),
+    //       resolve(parent, args) {
+    //         return Client.find();
+    //       },
+    //     },
+    client: {
+      type: ClientType,
+      args: { id: { type: GraphQLID } },
+      resolve(parent, args) {
+        return client.findById((client) => client.id === args.id);
+      },
+    },
+  },
+});
+
+module.exports = new GraphQLSchema({
+  query: RootQuery,
+});
