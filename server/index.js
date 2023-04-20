0071111f38ff75d82ee0eb796e8d87344eb5e793
mohammadreza memar <moreza.memar@gmail.com>
2023-04-20 15:04:12 -0500

    mongodb connected

--- server/index.js
+++ server/index.js
@@ -1,11 +1,15 @@
-// const { config } = require("dotenv");
 const express = require("express");
+const colors = require("colors");
 require("dotenv").config();
 const { graphqlHTTP } = require("express-graphql");
 const schema = require("./schema/schema.js");
+const connectDB = require("./config/db.js");
 const port = process.env.PORT || 8080;
 
 const app = express();
+
+//connect to database
+connectDB();
 
 app.use(
   "/graphql",
