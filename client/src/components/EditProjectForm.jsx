b30341bda103046313e3b491410ce2d5b4e9650b
mohammadreza memar <moreza.memar@gmail.com>
2023-04-24 13:24:59 -0500

    edit button working good

--- client/src/components/EditProjectForm.jsx
+++ client/src/components/EditProjectForm.jsx
@@ -1,16 +1,37 @@
 import { useState } from 'react';
 import { useMutation } from '@apollo/client';
-import { EDIT_PROJECT } from '../mutations/projectMutations';
 import { GET_PROJECTS } from '../queries/projectQueries';
+import { UPDATE_PROJECT } from '../mutations/projectMutations';
 
 export default function EditProjectForm({ project }) {
     const [name, setName] = useState(project.name);
     const [description, setDescription] = useState(project.description);
     const [status, setStatus] = useState('');
+
+    const [updateProject] = useMutation(UPDATE_PROJECT, {
+        variables: { id: project.id, name, description, status },
+        refetchQueries: [{
+            query: GET_PROJECTS, variables: {
+                id: project.id
+            }
+        }],
+    })
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+
+        if (!name || !description || !status) {
+            return alert('All fields are required!');
+        }
+
+        updateProject(name, description, status);
+    }
+
+
     return (
         <div className='mt-5'>
             <h3>Update Project Details</h3>
-            <form>
+            <form onSubmit={onSubmit}>
                 <div className='mb-3'>
                     <label className='form-label'>Name</label>
                     <input
@@ -43,6 +64,7 @@ export default function EditProjectForm({ project }) {
                         <option value='completed'>Completed</option>
                     </select>
                 </div>
+                <button type='submit' className='btn btn-primary'>Submit</button>
 
             </form>
 
